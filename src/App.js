8c4e706e848695aaac6d389e45923d73cb6bdbd2
chughes4 <chughes4life@gmail.com>
2020-09-01 22:09:21 -0400

    Axios added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Axios from "axios";
 
+const api = {
+  key: "55d72adf9b689b6d5db5f646d65e6851",
+  base: "http://api.openweathermap.org/data/2.5/"
+};
+//imperial
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  // const search = evt => {
+  //   if (evt.key === "Enter") {
+  //     fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+  //       .then(res => res.json())
+  //       .then(result => {
+  //         setWeather(result);
+  //         setQuery("");
+  //         console.log(result);
+  //       });
+  //   }
+  // };
+  async function search(evt) {
+    if (evt.key === "Enter") {
+      await Axios.get(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+        .then(res => res.data)
+        .then(result => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+          // .then(function (response) {
+          //   // handle success
+          //   console.log(response);
+        })
+        .catch(error => console.log(error));
+    }
+  }
+
+  const dateBuilder = d => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={
+        typeof weather.main !== "undefined" ? (weather.main.temp > 63 ? "app warm" : "app") : "app"
+      }
+    >
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search!"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main == "undefined" ? (
+          <h1 className="h1">Enter a city to get weather</h1>
+        ) : (
+          ""
+        )}
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°F</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
     </div>
   );
 }
